cd2ae9738c14e76b0c62b07ba1968c105899f105
ralphatmeh <153554956+ralphatmeh@users.noreply.github.com>
2024-11-15 01:44:09 +0200

    Update app.module.ts

--- smf/apps/backend/src/app.module.ts
+++ smf/apps/backend/src/app.module.ts
@@ -1,25 +1,23 @@
-// src/app.module.ts
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './entities/User';  // Import the User entity
+import { User } from './entities/User'; 
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot({
-      dbName: 'your-database-name',   // e.g., 'testdb'
-      driver: PostgreSqlDriver,       // PostgreSQL type
-      user: 'your-db-user',           // Database user
-      password: 'your-db-password',   // Database password
-      host: 'localhost',              // Database host
-      port: 5432,                     // PostgreSQL default port
-      autoLoadEntities: true,         // Automatically load entities
-      entities: [User],               // Explicitly include the User entity
-      debug: true,                    // Enable SQL query debugging (optional)
+      dbName: 'Vip_Test',   
+      driver: PostgreSqlDriver,      
+      user: 'admin',          
+      password: 'admin',  
+      host: 'localhost',             
+      port: 5432,                    
+      autoLoadEntities: true,         
+      entities: [User],               
+      debug: true,                    
     }),
-    // MikroOrmModule.forFeature([User]),  // Remove this line
   ],
   controllers: [AppController],
   providers: [AppService],
